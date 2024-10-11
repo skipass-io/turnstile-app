20c20feb62b3dd37cafdc9e9dbf5d608667a6997
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-11 13:35:25 +0300

    new layout, change bg for all window, const width & height for picam2

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ from qgl_picam2_widget import QGlPicamera2
 def request_callback(request):
     label_top.setText("Top")
     label_bottom.setText("Bottom")
+    window.setStyleSheet('background-color: green;')
     # label_top.setText("".join(f"{k}: {v}\n" for k, v in request.get_metadata().items()))
 
 
@@ -26,24 +27,34 @@ def cleanup():
 
 picam2 = Picamera2()
 picam2.post_callback = request_callback
-picam2.configure(picam2.create_preview_configuration(main={"size": (800, 600)}))
+PICAM2_WIDTH=1024
+PICAM2_HEIGHT=768
+picam2.configure(
+    picam2.create_preview_configuration(main={"size": (PICAM2_WIDTH, PICAM2_HEIGHT)})
+)
 app = QApplication([])
 
 
-qpicamera2 = QGlPicamera2(picam2, width=800, height=600)
+qpicamera2 = QGlPicamera2(picam2, width=PICAM2_WIDTH, height=PICAM2_HEIGHT)
 label_top = QLabel()
 label_bottom = QLabel()
 window = QWidget()
 window.setWindowTitle("Qt Picamera2 App")
+window.setAttribute(QtCore.Qt.WA_StyledBackground, True)
 
-label_top.setFixedHeight(150)
+
+label_top.setFixedHeight(80)
 label_top.setAlignment(QtCore.Qt.AlignTop)
+
+label_bottom.setFixedHeight(40)
+
 layout_v = QVBoxLayout()
 layout_v.addWidget(label_top)
 layout_v.addWidget(qpicamera2)
 layout_v.addWidget(label_bottom)
-window.resize(1200, 600)
+# window.resize(1200, 600)
 window.setLayout(layout_v)
+window.showFullScreen()
 
 
 def main():
@@ -51,6 +62,7 @@ def main():
     window.show()
     app.exec_()
     picam2.stop()
+
 
 if __name__ == "__main__":
     main()
