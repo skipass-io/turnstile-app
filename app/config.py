4e95015ed8e4ff039361c7443ca3709b7af95eb3
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-14 17:24:33 +0300

    create settings for GuestRecognition & path haarcascade file for face detector

--- app/config.py
+++ app/config.py
@@ -1,10 +1,16 @@
 from pathlib import Path
 import os
 
-#settings for tensorflow logging
+# settings for tensorflow logging
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 BASE_DIR = Path(__file__).parent
+
+
+class GuestRecognitionSettings:
+    haarcascade_file = "haarcascade_frontalface_default.xml"
+    haarcascade_path = f"{BASE_DIR}/app/data/haarcascades/{haarcascade_file}"
+
 
 class DBSQLiteSettings:
     scaffold_sql = f"{BASE_DIR}/app/scaffod.sql"
