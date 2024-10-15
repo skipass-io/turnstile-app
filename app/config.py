660f43cebcda197b1a5ed62ca3ded11bc6573cb9
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-15 17:47:59 +0300

    ref config: new comments for groups of settings

--- app/config.py
+++ app/config.py
@@ -7,6 +7,14 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 BASE_DIR = Path(__file__).parent.parent
 
 
+# DB Settings
+class DBSQLiteSettings:
+    scaffold_sql = f"{BASE_DIR}/app/scaffod.sql"
+    db_name = "db.sqlite"
+    db_path = f"{BASE_DIR}/db/{db_name}"
+
+
+# Guest Recognition Settings
 class FaceDetectorSettings:
     scale_factor = 1.3
     min_neighbors = 5
@@ -17,9 +25,3 @@ class GuestRecognitionSettings:
     haarcascade_file = "haarcascade_frontalface_default.xml"
     haarcascade_path = f"{BASE_DIR}/app/data/haarcascades/{haarcascade_file}"
     face_detector_settings = FaceDetectorSettings()
-
-
-class DBSQLiteSettings:
-    scaffold_sql = f"{BASE_DIR}/app/scaffod.sql"
-    db_name = "db.sqlite"
-    db_path = f"{BASE_DIR}/db/{db_name}"
