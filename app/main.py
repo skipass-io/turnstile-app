6814fd2a92e526e8559308487d1651c7d8a62365
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-15 17:53:36 +0300

    feat main: add guest_recognition & qt app can found face from picam; ref main: some imports and new struct of code for guest_recognition

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-import numpy as np
+
 from PySide2 import QtCore
 from PySide2.QtWidgets import (
     QApplication,
@@ -8,16 +8,26 @@ from PySide2.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
-
-from picamera2 import Picamera2
+from picamera2 import Picamera2, MappedArray
 from qgl_picam2_widget import QGlPicamera2
+
+from guest_recognition import GuestRecognition
+
+# TODO: target for vetical screen size 720:1280
+PICAM2_WIDTH = 720  
+PICAM2_HEIGHT = 768  
+
+
+guest_recognition = GuestRecognition(frame_size=(PICAM2_WIDTH, PICAM2_HEIGHT))
 
 
 def request_callback(request):
-    label_left.setText("Left")
-    label_right.setText("Right")
+    with MappedArray(request, "main") as m:
+        guest_recognition.set_frame(m)
+        guest_recognition.find_faces()
+    label_left.setText("Left side")
+    label_right.setText("Right side")
     window.setStyleSheet("background-color: #ffffff;")
-
 
 
 def cleanup():
@@ -26,9 +36,6 @@ def cleanup():
 
 picam2 = Picamera2()
 picam2.post_callback = request_callback
-PICAM2_WIDTH = 720 #720 #1366
-PICAM2_HEIGHT = 768 #768
-# 1366 x 768
 picam2.configure(
     picam2.create_preview_configuration(main={"size": (PICAM2_WIDTH, PICAM2_HEIGHT)})
 )
@@ -36,7 +43,11 @@ app = QApplication(sys.argv)
 
 
 qpicamera2 = QGlPicamera2(
-    picam2, width=PICAM2_WIDTH, height=PICAM2_HEIGHT, bg_colour=(255, 255, 255), keep_ar=False
+    picam2,
+    width=PICAM2_WIDTH,
+    height=PICAM2_HEIGHT,
+    bg_colour=(255, 255, 255),
+    keep_ar=False,
 )
 label_left = QLabel()
 label_right = QLabel()
@@ -55,7 +66,6 @@ layout_h = QHBoxLayout()
 layout_h.addWidget(label_left)
 layout_h.addWidget(qpicamera2)
 layout_h.addWidget(label_right)
-# window.resize(1200, 600)
 window.setLayout(layout_h)
 window.showFullScreen()
 
