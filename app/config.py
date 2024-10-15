6bead2fa919a51ea15d975decf1dcceaefa13292
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-15 17:47:07 +0300

    ref&feat(config): ref BASE_DIR, add face_detector_settings

--- app/config.py
+++ app/config.py
@@ -4,12 +4,19 @@ import os
 # settings for tensorflow logging
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-BASE_DIR = Path(__file__).parent
+BASE_DIR = Path(__file__).parent.parent
+
+
+class FaceDetectorSettings:
+    scale_factor = 1.3
+    min_neighbors = 5
+    scalar_face_detect = 7
 
 
 class GuestRecognitionSettings:
     haarcascade_file = "haarcascade_frontalface_default.xml"
     haarcascade_path = f"{BASE_DIR}/app/data/haarcascades/{haarcascade_file}"
+    face_detector_settings = FaceDetectorSettings()
 
 
 class DBSQLiteSettings:
