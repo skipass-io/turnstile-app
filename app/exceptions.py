8ebd16f043d3ad5351d12be99a8ea8d7a7b7ec77
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-15 21:27:04 +0300

    ref(exceptions): refactoring message NotCorrectStatusGR

--- app/exceptions.py
+++ app/exceptions.py
@@ -5,5 +5,5 @@ class NotCorrectStatusGR(Exception):
     """Raise if NotCorrectStatusGR"""
 
     def __init__(self, current_status, correct_status):
-        message = f"Current status: {current_status}; Expected: {correct_status}"
+        message = f"Current status: {current_status}; Expected status: {correct_status}"
         super().__init__(message)
