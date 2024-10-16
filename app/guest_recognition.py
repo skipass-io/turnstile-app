c53e6d7ebb4a825575f9fa27ab01ccb9cf14d467
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-16 20:09:28 +0300

    feat&ref(guest_recognition): ref - Internal methods higher, external methods lower, feat - method "run" for processing PiCamera frame

--- app/guest_recognition.py
+++ app/guest_recognition.py
@@ -1,4 +1,7 @@
 import cv2 as cv
+import numpy as np
+from keras_facenet import FaceNet
+from sklearn.preprocessing import LabelEncoder
 from pyzbar.pyzbar import decode as pyzbar_decoder
 from pyzbar.pyzbar import Decoded as PyzbarDecoded
 
@@ -10,26 +13,25 @@ _settings = GuestRecognitionSettings()
 
 
 class GuestRecognition:
-    def __init__(self, frame_size):
-        if (not frame_size) or (len(frame_size) != 2):
-            raise NotCorrectFrameSizeGR(frame_size=frame_size)
+    """That's how we let the guests through"""
 
-        self.status = None
-        self.frame = None
-        self.width = frame_size[0]
-        self.height = frame_size[1]
-        self.cv_rgb = None
-        self.cv_gray = None
-        self.face_detector = cv.CascadeClassifier(_settings.haarcascade_path)
-        self.qr_decoder = pyzbar_decoder
+    def _cv_img(self, color):
+        match color:
+            case "rgb":
+                cv_color = cv.COLOR_BGR2RGB
+            case "gray":
+                cv_color = cv.COLOR_BGR2GRAY
+            case _:
+                raise Exception("Need color for get_cv_img: gray or rgb")
+        return cv.cvtColor(self.frame, cv_color)
 
-    def set_frame(self, mapped_array):
+    def _set_frame(self, mapped_array):
         self.frame = mapped_array.array
         self.cv_rgb = self._cv_img("rgb")
         self.cv_gray = self._cv_img("gray")
         self.status = "set_frame"
 
-    def find_qrcodes(self):
+    def _find_qrcodes(self):
         correct_status = "set_frame"
         if not self.status == correct_status:
             raise NotCorrectStatusGR(
@@ -40,15 +42,14 @@ class GuestRecognition:
         codes = self.qr_decoder(self.cv_gray)
         self._draw_rectangle(codes)
 
-    def find_faces(self):
+    def _find_faces(self):
         correct_status = "set_frame"
+        fd_settings = _settings.face_detector_settings
         if not self.status == correct_status:
             raise NotCorrectStatusGR(
                 current_status=self.status,
                 correct_status=correct_status,
             )
-
-        fd_settings = _settings.face_detector_settings
 
         found_faces = self.face_detector.detectMultiScale(
             image=self.cv_gray,
@@ -68,12 +69,24 @@ class GuestRecognition:
             x, y, w, h = obj
             cv.rectangle(self.frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
 
-    def _cv_img(self, color):
-        match color:
-            case "rgb":
-                cv_color = cv.COLOR_BGR2RGB
-            case "gray":
-                cv_color = cv.COLOR_BGR2GRAY
-            case _:
-                raise Exception("Need color for get_cv_img: gray or rgb")
-        return cv.cvtColor(self.frame, cv_color)
+    def run(self, mapped_array):
+        """Processing PiCamera frame"""
+        self._set_frame(mapped_array)
+        self._find_qrcodes()
+        self._find_faces()
+
+    def __init__(self, frame_size):
+        if (not frame_size) or (len(frame_size) != 2):
+            raise NotCorrectFrameSizeGR(frame_size=frame_size)
+
+        self.status = None
+        self.frame = None
+        self.width = frame_size[0]
+        self.height = frame_size[1]
+        self.cv_rgb = None
+        self.cv_gray = None
+        self.face_detector = cv.CascadeClassifier(_settings.haarcascade_path)
+        self.qr_decoder = pyzbar_decoder
+        self.facenet = FaceNet()
+        self.faces_embeddings = None
+        self.svm_model = None
