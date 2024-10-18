5f75c4715d8b51b14e12e8d90c818d8524ac2212
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-18 15:45:42 +0300

    new(guest_recognition:statuses): removing `StatusGR` class from the main `guest_recognition` implementation

---
+++ app/guest_recognition/statuses.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class StatusGR(Enum):
+    """
+    Statuses for GuestRecognition (Finite-State Machine)
+    ---
+    - `GET_READY`:
+    - `NOT_READY`:
+    - `ERROR`:
+    - `SEARCHING`:
+    - `PROCESSING`:
+    - `ALLOWED`:
+    - `NOT_ALLOWED`:
+    """
+
+    GET_READY = "get_ready"
+    NOT_READY = "not_ready"
+    ERROR = "error"
+    SEARCHING = "searching"
+    PROCESSING = "processing"
+    ALLOWED = "allowed"
+    NOT_ALLOWED = "not_allowed"
