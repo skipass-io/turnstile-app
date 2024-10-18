76be964823721256069ad29d6f709a368158130a
Kirill Zagoskin <kirillzagoskin.work@gmail.com>
2024-10-18 22:41:30 +0300

    ref(StatusFSM): add status `GET_CLOSER`, order statuses by workflow

--- app/guest_recognition/status_fsm.py
+++ app/guest_recognition/status_fsm.py
@@ -5,21 +5,23 @@ class StatusFSM(Enum):
     """
     Statuses for GuestRecognition (Finite-State Machine)
     ---
-    - `GET_READY`:
     - `NOT_READY`:
-    - `ERROR`:
+    - `GET_READY`:
     - `SEARCHING`:
+    - `GET_CLOSER`: 
     - `QRCODE_SCANNING`:
     - `FACE_RECOGNITION`:
     - `ALLOWED`:
     - `NOT_ALLOWED`:
+    - `ERROR`:
     """
 
-    GET_READY = "get_ready"
     NOT_READY = "not_ready"
-    ERROR = "error"
+    GET_READY = "get_ready"
     SEARCHING = "searching"
+    GET_CLOSER = "get_closer"
     QRCODE_SCANNING = "qrcode_scanning"
     FACE_RECOGNITION = "face_recognition"
     ALLOWED = "allowed"
     NOT_ALLOWED = "not_allowed"
+    ERROR = "error"
