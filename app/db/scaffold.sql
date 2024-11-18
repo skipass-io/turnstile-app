fe90ed81a52d43abbbc08629750f5084d5745742
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-18 22:56:32 +0300

    feat(app/db/scaffold.sql): Created in the initialization `scaffold.sql`- `app_settings` table with default settings values

--- app/db/scaffold.sql
+++ app/db/scaffold.sql
@@ -1,36 +1,12 @@
-create table config(
-    workday datetime primary key,
-    start_workday datetime,
-    end_workday datetime,
-);
+CREATE TABLE app_settings(
+    id integer primary key autoincrement,
+    show_performance boolean not null,
+    gr_level_a numeric not null,
+    gr_level_b numeric not null,
+    gr_level_c numeric not null,
+    gr_labels numeric not null,
+    gr_percent numeric not null
+); 
 
-create table skipasses( 
-    label varchar(255) primary key,
-    start_slot datetime,
-    end_slot datetime,
-    type_slot integer,
-);
-
-create table passages( 
-    id integer primary key,
-    time_passage datetime,
-    FOREIGN KEY(skipass_label) REFERENCES skipasses(label),
-    is_open boolean,
-);
-
-insert into config(workday, start_workday, end_workday) 
-values 
-    ("2024-10-13", "2024-10-13 10:00:00", "2024-10-13 18:00:00"),
-    ("2024-10-14", "2024-10-14 10:00:00", "2024-10-14 18:00:00"),
-    ("2024-10-15", "2024-10-15 10:00:00", "2024-10-15 18:00:00"),
-    ("2024-10-16", "2024-10-16 10:00:00", "2024-10-16 18:00:00"),
-    ("2024-10-17", "2024-10-17 10:00:00", "2024-10-17 18:00:00"),
-    ("2024-10-18", "2024-10-18 10:00:00", "2024-10-18 18:00:00"),
-    ("2024-10-19", "2024-10-19 10:00:00", "2024-10-19 18:00:00"),
-    ("2024-10-20", "2024-10-20 10:00:00", "2024-10-20 18:00:00"),
-    ("2024-10-21", "2024-10-21 10:00:00", "2024-10-21 18:00:00");
-
-insert into skipasses (label, start_slot, end_slot, type_slot)
-values
-    ("kirill_zagoskin", "2024-10-13 10:00:00", "2024-10-13 16:00:00", 0);
-
+INSERT INTO app_settings (show_performance, gr_level_a, gr_level_b, gr_level_c, gr_labels, gr_percent)
+values (0, 136, 180, 250, 20, 85)
