bcd68e05bd7edf21324ee3214aa70129f4a980b1
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-19 11:08:23 +0300

    ref(guest_recognition/open_cv): ref `output_face` with new settings params `level_b` & `level_c` as stage side of face rectangle

--- app/guest_recognition/open_cv.py
+++ app/guest_recognition/open_cv.py
@@ -15,27 +15,27 @@ def output_performance(frame, params, width, height):
     )
 
 
-def output_face(frame, face, area_start, area_back, performance):
+def output_face(frame, face, level_b, level_c, show_performance):
     x, y, w, h = face
-    area = int(w * h / 1000)
-    if area < area_start:
+    area = int(w * h)
+    if area < (level_b * level_b):
         color = (17, 151, 228)  # Blue
         label = (
-            f"{area} area - closer"
-            if performance == "True"
-            else f"{int(area / area_start * 100)}% - closer"
+            f"{area} area: LEVEL A"
+            if show_performance
+            else f"{int(area / (level_b * level_b) * 100)}% - get closer"
         )
         thinkness = 3
-    elif area_start <= area < area_back:
+    elif (level_b * level_b) <= area < (level_c * level_c):
         color = (194, 51, 255)  # Pink
-        label = f"{area} area - recognition" if performance == "True" else f"Wait"
+        label = f"{area} area: LEVEL B" if show_performance else f"Wait..."
         thinkness = 4
     else:
         color = (255, 255, 255)  # White
         label = (
-            f"{area} area - back"
-            if performance == "True"
-            else f"{int(area / area_back * 100)}% - back"
+            f"{area} area: LEVEL C"
+            if show_performance
+            else f"{int(area / (level_c * level_c) * 100)}% - get back"
         )
         thinkness = -1
 
