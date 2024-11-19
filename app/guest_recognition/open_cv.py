800201ff885bdd063c15488d3d5d925999b661a1
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-20 01:48:23 +0300

    ref(guest_recognition/open_cv): TODO #21, Square LEVEL_A, LEVEL_B, LEVEL_C to compare the areas, big full rect on `LEVEL_C`

--- app/guest_recognition/open_cv.py
+++ app/guest_recognition/open_cv.py
@@ -15,29 +15,39 @@ def output_performance(frame, params, width, height):
     )
 
 
-def output_face(frame, face, level_b, level_c, show_performance):
+def output_face(
+    frame,
+    face,
+    facearea_level_b,
+    facearea_level_c,
+    show_performance,
+):
     x, y, w, h = face
-    area = int(w * h)
-    if area < (level_b * level_b):
+    facearea = w * h
+    if facearea < facearea_level_b:
         color = (17, 151, 228)  # Blue
         label = (
-            f"{area} area: LEVEL A"
+            f"{facearea} area: LEVEL A"
             if show_performance
-            else f"{int(area / (level_b * level_b) * 100)}% - get closer"
+            else f"{int(facearea / facearea_level_b * 100)}% - get closer"
         )
         thinkness = 3
-    elif (level_b * level_b) <= area < (level_c * level_c):
+    elif facearea_level_b <= facearea < facearea_level_c:
         color = (194, 51, 255)  # Pink
-        label = f"{area} area: LEVEL B" if show_performance else f"Wait..."
+        label = f"{facearea} area: LEVEL B" if show_performance else f"Wait..."
         thinkness = 4
     else:
         color = (255, 255, 255)  # White
         label = (
-            f"{area} area: LEVEL C"
+            f"{facearea} area: LEVEL C"
             if show_performance
-            else f"{int(area / (level_c * level_c) * 100)}% - get back"
+            else f"{int(facearea / facearea_level_c * 100)}% - get back"
         )
         thinkness = -1
+        x -= 25
+        y -= 25
+        w += 50
+        h += 50
 
     cv.rectangle(frame, (x, y), (x + w, y + h), color, thinkness)
     cv.putText(
