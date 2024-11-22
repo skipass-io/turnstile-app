8b59091a3a68e250ee725e70bf9b4b2b2af52ddc
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-22 23:00:35 +0300

    feat(db): add `gr_face_detector`, `gr_iteration` columns into `app_settings` table

--- app/db/scaffold.sql
+++ app/db/scaffold.sql
@@ -1,12 +1,23 @@
 CREATE TABLE app_settings(
     id integer primary key autoincrement,
     show_performance boolean not null,
+    gr_face_detector numeric not null,
     gr_level_a numeric not null,
     gr_level_b numeric not null,
     gr_level_c numeric not null,
     gr_labels numeric not null,
-    gr_percent numeric not null
+    gr_percent numeric not null,
+    gr_iteration numeric not null
 ); 
 
-INSERT INTO app_settings (show_performance, gr_level_a, gr_level_b, gr_level_c, gr_labels, gr_percent)
-values (1, 136, 180, 250, 20, 85)
+INSERT INTO app_settings (
+    show_performance, 
+    gr_face_detector,
+    gr_level_a, 
+    gr_level_b, 
+    gr_level_c, 
+    gr_labels, 
+    gr_percent,
+    gr_iteration
+)
+values (1, 0, 136, 180, 250, 20, 85, 3)
