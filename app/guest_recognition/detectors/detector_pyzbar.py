c46f2bf13f2ca7de5e46e289716a8c8d304ffe57
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-23 22:57:41 +0300

    ref(DetectorPyzbar): `detect_qrcode` return `qr_code, `decode_qrcode` return decode `qr_code`, `get_coords_qrcode` return rect `qr_code`

--- app/guest_recognition/detectors/detector_pyzbar.py
+++ app/guest_recognition/detectors/detector_pyzbar.py
@@ -9,19 +9,25 @@ class DetectorPyzbar:
     def detect_qrcode(self, cv_gray):
         """
         QR code detection on the frame
-        (if there is more than one QR code, 
+        (if there is more than one QR code,
         the largest one / closest to the frame will be returned)
         """
-        qr_codes = [qr for qr in self.decoder(cv_gray) if qr.type == "QRCODE"]
+        qr_codes = [qr for qr in self.decoder(cv_gray.tobytes()) if qr.type == "QRCODE"]
         if len(qr_codes) == 0:
             return
         qr_code = self._get_qrcode(qr_codes)
+        return qr_code
+
+    def decode_qrcode(self, qr_code):
         return qr_code.data.decode("utf-8")
+
+    def get_coords_qrcode(self, qr_code):
+        return qr_code.rect
 
     def _get_qrcode(self, qr_codes):
         """
-        Returns a QR Code from the array of detected `qr_codes`. 
-        (if more than one QR Code is detected, 
+        Returns a QR Code from the array of detected `qr_codes`.
+        (if more than one QR Code is detected,
         the one with the larger area will be returned.)
         """
         if len(qr_codes) == 1:
