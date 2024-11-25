8ea417a934d1ea0443417629d2f16d0f03a930bf
3agoskin <kirillzagoskin.work@gmail.com>
2024-11-25 22:52:01 +0300

    ref&feat(db/scaffold.sql): add value type in column name in `app_settings` table, add `gr_allowed_time_sec`, `gr_passage_time_sec`, `gr_not_allowed_time_sec` columns in `app_settings`

--- app/db/scaffold.sql
+++ app/db/scaffold.sql
@@ -2,12 +2,15 @@ CREATE TABLE app_settings(
     id integer primary key autoincrement,
     show_performance boolean not null,
     gr_face_detector numeric not null,
-    gr_level_a numeric not null,
-    gr_level_b numeric not null,
-    gr_level_c numeric not null,
-    gr_labels numeric not null,
-    gr_percent numeric not null,
-    gr_iteration numeric not null
+    gr_level_a_side numeric not null,
+    gr_level_b_side numeric not null,
+    gr_level_c_side numeric not null,
+    gr_labels_count numeric not null,
+    gr_percent_required numeric not null,
+    gr_iteration_count numeric not null,
+    gr_allowed_time_sec numeric not null,
+    gr_passage_time_sec numeric not null,
+    gr_not_allowed_time_sec numeric not null,
 ); 
 
 CREATE TABLE skipass(
@@ -21,11 +24,26 @@ CREATE TABLE skipass(
 INSERT INTO app_settings (
     show_performance, 
     gr_face_detector,
-    gr_level_a, 
-    gr_level_b, 
-    gr_level_c, 
-    gr_labels, 
-    gr_percent,
-    gr_iteration
+    gr_level_a_side, 
+    gr_level_b_side, 
+    gr_level_c_side, 
+    gr_labels_count, 
+    gr_percent_required, 
+    gr_iteration_count,
+    gr_allowed_time_sec,
+    gr_passage_time_sec,
+    gr_not_allowed_time_sec
 )
-values (1, 0, 136, 180, 250, 20, 85, 3)
+values (
+    1, 
+    0, 
+    136, 
+    180, 
+    250, 
+    20, 
+    85, 
+    2,
+    6,
+    1.5,
+    10
+)
