bc40fda884981a1a9fc0881dc0e58b35d8c95adc
Polina Zmanovskaya <polina.j4@gmail.com>
2025-03-24 17:29:29 +0100

    picam2.set_controls test

--- app/qt/app.py
+++ app/qt/app.py
@@ -57,7 +57,7 @@ picam2.configure(
 
 # rpi-camera-settings
 picam2.set_controls({"AeEnable": True})
-picam2.set_controls({"ExposureValue": 1.0})
+picam2.set_controls({"ExposureValue": 0.0})
 picam2.set_controls({"AeConstraintMode": controls.AeConstraintModeEnum.Shadows})
 picam2.set_controls({"AeMeteringMode": controls.AeMeteringModeEnum.Matrix})
 picam2.set_controls({"AeExposureMode": controls.AeExposureModeEnum.Normal})
