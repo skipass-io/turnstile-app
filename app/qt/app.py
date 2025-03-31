3a7b3721223068eab04137732f228f1fe5396adf
Polina Zmanovskaya <polina.j4@gmail.com>
2025-03-31 18:02:58 +0200

    picam2.set_controls test

--- app/qt/app.py
+++ app/qt/app.py
@@ -62,7 +62,7 @@ picam2.set_controls({"AeEnable": False})
 #picam2.set_controls({"AeMeteringMode": controls.AeMeteringModeEnum.Matrix})
 picam2.set_controls({"Contrast": 1.0})
 picam2.set_controls({"Brightness": 1.0})
-picam2.set_controls({"ExposureTime": 500})
+picam2.set_controls({"ExposureTime": 16500})
 picam2.set_controls({"AnalogueGain": 1.0})
 #picam2.set_controls({"AeExposureMode": controls.AeExposureModeEnum.Normal})
 
