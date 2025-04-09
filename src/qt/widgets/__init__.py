b9b653720c8e4093162fa9f26ff7a9bc6b222142
3agoskin <kirillzagoskin.work@gmail.com>
2025-04-09 12:58:07 +0300

    ref(src/qt/widgets): remove `RightWidget`

--- src/qt/widgets/__init__.py
+++ src/qt/widgets/__init__.py
@@ -1,6 +1,9 @@
-__all__ = ["CentralWidget", "LeftWidget", "RightWidget", "QGlPicamera2"]
+__all__ = (
+    "CentralWidget",
+    "LeftWidget",
+    "QGlPicamera2",
+)
 
 from .central_widget import CentralWidget
 from .left_widget import LeftWidget
-from .right_widget import RightWidget
 from .qgl_picam2_widget import QGlPicamera2
