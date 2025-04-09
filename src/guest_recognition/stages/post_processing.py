1c50e6058296c426f84e48cd6d584bc1e8a51fb6
3agoskin <kirillzagoskin.work@gmail.com>
2025-04-09 13:03:39 +0300

    ref(src/guest_recognition): `post_processing` correct work

--- src/guest_recognition/stages/post_processing.py
+++ src/guest_recognition/stages/post_processing.py
@@ -1,11 +1,11 @@
 import io
 import random
 import time
-from PIL import Image
+import cv2 as cv
 
 from core import settings
 from core.schemas import TurnstileSettings
-from guest_recognition import StatusFSM
+from guest_recognition.status_fsm import StatusFSM
 from guest_recognition.actions import DB, Server
 from guest_recognition.detectors import DetectorPyzbar
 from guest_recognition.utils import FPS, OpenCV, TurnstileGPIO
@@ -36,9 +36,11 @@ class PostProcessing:
         self.face_frames = []
         self.FACE_FRAMES_COUNT = None
 
-        self.turnstile_settings: TurnstileSettings = TurnstileSettings(
+        self.turnstile_settings_default: TurnstileSettings = TurnstileSettings(
             **settings.turnstile.default.model_dump()
         )
+
+        self.turnstile_settings = self.turnstile_settings_default
 
         # Post Processing Data Output
         self.status = None
@@ -61,17 +63,24 @@ class PostProcessing:
             frame=frame,
             cv_gray=cv_gray,
             cv_rgb=cv_rgb,
-            detected_qrcode=detected_qrcode,
             detected_face=detected_face,
         )
-        if self.detected_qrcode:
-            self._stage_qrcode(status=status, detected_qrcode=detected_qrcode)
+        if detected_qrcode:
+            self._stage_qrcode(
+                status=status,
+                detected_qrcode=detected_qrcode,
+            )
         elif detected_face:
-            self._stage_face()
+            self._stage_face(
+                status=status,
+                detected_face=detected_face,
+            )
         else:
-            self._stage_nothing()
+            self._stage_nothing(
+                status=status,
+            )
 
-        if self.turnstile_settings.show_perfomance:
+        if self.turnstile_settings.show_performance:
             self._stage_perfomance()
 
     # STAGE_PART
@@ -96,16 +105,16 @@ class PostProcessing:
 
     def _stage_face(self, status, detected_face):
         self.detected_face = detected_face
-        if self.detected_face["area"] < self.turnstile_settings.gr_level_b:
-            self._stage_face_level_a()
+        if self.detected_face["side"] < self.turnstile_settings.gr_level_b:
+            self._stage_face_level_a(status=status)
         elif (
             self.turnstile_settings.gr_level_b
-            <= self.detected_face["area"]
+            <= self.detected_face["side"]
             < self.turnstile_settings.gr_level_c
         ):
-            self._stage_face_level_b()
+            self._stage_face_level_b(status=status)
         else:
-            self._stage_face_level_c()
+            self._stage_face_level_c(status=status)
 
     def _stage_face_level_a(self, status):
         # Get Closer
@@ -119,7 +128,7 @@ class PostProcessing:
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_A,
                     progress=int(
-                        self.detected_face["area"]
+                        self.detected_face["side"]
                         / self.turnstile_settings.gr_level_b
                         * 100
                     ),
@@ -130,7 +139,7 @@ class PostProcessing:
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_A,
                     progress=int(
-                        self.detected_face["area"]
+                        self.detected_face["side"]
                         / self.turnstile_settings.gr_level_b
                         * 100
                     ),
@@ -138,7 +147,7 @@ class PostProcessing:
             case StatusFSM.ALLOWED:
                 self._stage_check_allowed_status()
             case StatusFSM.NOT_ALLOWED:
-                self._stage_check_not_allowed_time()
+                self._stage_check_not_allowed_status()
 
     def _stage_face_level_b(self, status):
         match status:
@@ -154,12 +163,10 @@ class PostProcessing:
                 )
             case StatusFSM.FACE_DETECTED_LEVEL_B:
 
-                face_frame = self._utils_face_frame_extract(
-                    self.cv_rgb, self.detected_face["rect"]
-                )
-                self.face_frames.append(face_frame)
+                self.face_frames.append(self.cv_rgb)
 
                 if len(self.face_frames) >= self.FACE_FRAMES_COUNT:
+                    print("_server_passage_request")
                     self._server_passage_request()
                 else:
                     self._set_status_and_progress(
@@ -174,7 +181,7 @@ class PostProcessing:
             case StatusFSM.ALLOWED:
                 self._stage_check_allowed_status()
             case StatusFSM.NOT_ALLOWED:
-                self._stage_check_not_allowed_time()
+                self._stage_check_not_allowed_status()
 
     def _stage_face_level_c(self, status):
         match status:
@@ -188,7 +195,7 @@ class PostProcessing:
                     status=StatusFSM.FACE_DETECTED_LEVEL_C,
                     progress=int(
                         self.turnstile_settings.gr_level_c
-                        / self.detected_face["area"]
+                        / self.detected_face["side"]
                         * 100
                     ),
                 )
@@ -199,21 +206,23 @@ class PostProcessing:
                     status=StatusFSM.FACE_DETECTED_LEVEL_C,
                     progress=int(
                         self.turnstile_settings.gr_level_c
-                        / self.detected_face["area"]
+                        / self.detected_face["side"]
                         * 100
                     ),
                 )
             case StatusFSM.ALLOWED:
                 self._stage_check_allowed_status()
             case StatusFSM.NOT_ALLOWED:
-                self._stage_check_not_allowed_time()
+                self._stage_check_not_allowed_status()
 
     def _stage_nothing(self, status):
         # TODO: give turnstile state to server
         match status:
             case StatusFSM.NOT_ACTIVE:
                 if turnstile_settings := self.server.turnstile_active():
-                    self.turnstile_settings = turnstile_settings
+                    self._set_turnstile_settings_update(
+                        turnstile_settings_update=turnstile_settings
+                    )
                     self._set_status_and_progress(
                         status=StatusFSM.DETECTING,
                     )
@@ -235,14 +244,15 @@ class PostProcessing:
                     status=StatusFSM.DETECTING,
                 )
             case StatusFSM.ALLOWED:
-                self._post_processing_check_allowed_status()
+                self._stage_check_allowed_status()
             case StatusFSM.NOT_ALLOWED:
-                self._post_processing_check_not_allowed_time()
+                self._stage_check_not_allowed_status()
 
     def _stage_perfomance(self):
         performance_params = dict()
-        performance_params["fps"] = self.fps.fps
+        performance_params["fps"] = self.fps.get_frames_per_second()
         performance_params["brt"] = self.open_cv.get_brightness(frame=self.frame)
+        performance_params["status"] = self.status.value
 
         self.open_cv.output_performance(
             frame=self.frame,
@@ -265,7 +275,7 @@ class PostProcessing:
                 progress=int(passing_time / self.PASSAGE_TIME_LIMIT * 100),
             )
 
-    def _stage_check_not_allowed_time(self):
+    def _stage_check_not_allowed_status(self):
         passing_time = time.time() - self.passage_time
 
         if passing_time >= self.PASSAGE_TIME_LIMIT:
@@ -286,7 +296,6 @@ class PostProcessing:
         frame,
         cv_gray,
         cv_rgb,
-        detected_qrcode,
         detected_face,
     ):
         # Data Input
@@ -295,7 +304,6 @@ class PostProcessing:
         self.frame = frame
         self.cv_gray = cv_gray
         self.cv_rgb = cv_rgb
-        self.detected_qrcode = detected_qrcode
         self.detected_face = detected_face
 
         # Utils
@@ -303,8 +311,10 @@ class PostProcessing:
 
     def _set_turnstile_settings_update(self, turnstile_settings_update):
         for name, value in turnstile_settings_update.model_dump(
-            exclude_unset=True
+            exclude_unset=True,
+            exclude_none=True,
         ).items():
+            self.turnstile_settings = self.turnstile_settings_default
             setattr(self.turnstile_settings, name, value)
 
     def _set_status_and_progress(self, status, progress=0):
@@ -320,8 +330,9 @@ class PostProcessing:
         if passage is None:
             # TODO: if server let down
             print("passage request is None")
+            self.passage_time = time.time()
             self._set_status_and_progress(
-                status=StatusFSM.DETECTING,  #  <- There should be an another status
+                status=StatusFSM.NOT_ALLOWED,  #  <- There should be an another status
             )
             return
 
@@ -340,6 +351,7 @@ class PostProcessing:
                 status=StatusFSM.ALLOWED,
             )
         else:
+            self.passage_time = time.time()
             self._set_status_and_progress(
                 status=StatusFSM.NOT_ALLOWED,
             )
@@ -360,20 +372,23 @@ class PostProcessing:
     def _utils_set_face_frames_count(self):
         self.passage_start = time.time()
         self.FACE_FRAMES_COUNT = int(
-            self.fps.fps * 0.8
+            self.fps.get_frames_per_second() * 0.8
         )  # <- 80% of perfomance in 1 sec
 
-    def _utils_face_frame_extract(self, cv_rgb, detected_face):
-        x, y, w, h = detected_face["rect"]
-        return cv_rgb[y : y + h, x : x + w]
-
     def _utils_get_frames_for_request(self):
+        print("_utils_get_frames_for_request")
         k = int(len(self.face_frames) * 0.25)
-        faces = random.sample(population=self.face_frames, k=k)
+        faces = random.sample(population=self.face_frames[3:], k=k)
         frames = []
-        for face in faces:
-            pil_img = Image.fromarray(face)
-            buf = io.BytesIO()
-            pil_img.save(buf, format="PNG")
-            frames.append(buf.getvalue())
+        for i, face in enumerate(faces):
+            ext, buf = cv.imencode(f"frame{i}.png", face)
+            if ext:
+                bytes_io = io.BytesIO(buf)
+                frames.append(
+                    (
+                        "frames",
+                        (f"frame{i}.png", bytes_io.getvalue(), "image/png"),
+                    )
+                )
+
         return frames
