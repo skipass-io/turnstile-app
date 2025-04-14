b0ae07b44fb415d2f91c80e27bd4fa8efaefef20
3agoskin <kirillzagoskin.work@gmail.com>
2025-04-14 12:37:19 +0300

    feat(src/guest_recognition/stages/post_processing): new logic with svm model on rpi

--- src/guest_recognition/stages/post_processing.py
+++ src/guest_recognition/stages/post_processing.py
@@ -1,12 +1,13 @@
 import io
-import random
 import time
+from datetime import datetime
 import cv2 as cv
 
 from core import settings
 from core.config import BASEDIR
 from core.schemas import TurnstileSettings
 from guest_recognition.status_fsm import StatusFSM
+from guest_recognition.svm_model import SVMModel
 from guest_recognition.actions import DB, Server
 from guest_recognition.detectors import DetectorPyzbar
 from guest_recognition.utils import FPS, OpenCV, TurnstileGPIO
@@ -14,6 +15,10 @@ from guest_recognition.utils import FPS, OpenCV, TurnstileGPIO
 
 class PostProcessing:
     def __init__(self):
+        # SVM Model
+
+        self.svm_model = SVMModel()
+
         # Detectors
         self.pyzbar = DetectorPyzbar()
 
@@ -34,8 +39,9 @@ class PostProcessing:
         self.passage_start = None
         self.passage_time = None
         self.PASSAGE_TIME_LIMIT = settings.turnstile.passage_time_limit
-        self.face_frames = []
-        self.FACE_FRAMES_COUNT = None
+        self.labels = []
+        self.MAX_LABELS_COUNT = settings.turnstile.labels_count
+        self.FREQUENT_LABEL_PERCENT = settings.turnstile.frequent_label_percent
 
         self.turnstile_settings_default: TurnstileSettings = TurnstileSettings(
             **settings.turnstile.default.model_dump()
@@ -58,7 +64,7 @@ class PostProcessing:
         detected_qrcode,
         detected_face,
     ):
-        self._set_data_input(
+        self._set_data_input(  # TODO: remove self
             width=width,
             height=height,
             frame=frame,
@@ -93,6 +99,7 @@ class PostProcessing:
                     token=qrcode_token
                 ):
                     self.db.set_token(token=qrcode_token)
+                    self.svm_model.download(server=self.server)
                     self._set_turnstile_settings_update(
                         turnstile_settings_update=turnstile_settings
                     )
@@ -129,18 +136,18 @@ class PostProcessing:
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_A,
                     progress=int(
-                        self.detected_face["side"]
+                        self.detected_face["side"]  # type: ignore
                         / self.turnstile_settings.gr_level_b
                         * 100
                     ),
                 )
 
             case StatusFSM.FACE_DETECTED_LEVEL_B:
-                self.face_frames = []  # full reset
+                self.labels = []  # full reset
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_A,
                     progress=int(
-                        self.detected_face["side"]
+                        self.detected_face["side"]  # type: ignore
                         / self.turnstile_settings.gr_level_b
                         * 100
                     ),
@@ -158,24 +165,33 @@ class PostProcessing:
                 | StatusFSM.FACE_DETECTED_LEVEL_C
                 # | StatusFSM.QRCODE_DETECTED TODO: add QRCODE Acts
             ):
-                self._utils_set_face_frames_count()
+                self.passage_start = time.time()
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_B,
                 )
             case StatusFSM.FACE_DETECTED_LEVEL_B:
+                label = self.svm_model.recognize(
+                    face_coords=self.detected_face["rect"],  # type: ignore
+                    cv_rgb=self.cv_rgb,
+                    server=self.server,
+                )
+                self.labels.append(label)
+                date = datetime.fromtimestamp(self.passage_start)  # type: ignore
+                self._save_frame(
+                    cv_rgb=self.cv_rgb,
+                    name=f"{date.isoformat(timespec='seconds')}--{len(self.labels)}",
+                )
 
-                self.face_frames.append(self.cv_rgb)
-
-                if len(self.face_frames) >= self.FACE_FRAMES_COUNT:
+                if len(self.labels) >= self.MAX_LABELS_COUNT:  # type: ignore
                     print("_server_passage_request")
-                    self._server_passage_request()
+                    self._server_passage_request()  # TODO: find frequent_label
                 else:
                     self._set_status_and_progress(
                         status=StatusFSM.FACE_DETECTED_LEVEL_B,
                         progress=int(
-                            len(self.face_frames)
-                            / self.FACE_FRAMES_COUNT
-                            * 90  # <- for pending
+                            len(self.labels)
+                            / self.MAX_LABELS_COUNT  # type: ignore
+                            * 95
                         ),
                     )
 
@@ -196,18 +212,18 @@ class PostProcessing:
                     status=StatusFSM.FACE_DETECTED_LEVEL_C,
                     progress=int(
                         self.turnstile_settings.gr_level_c
-                        / self.detected_face["side"]
+                        / self.detected_face["side"]  # type: ignore
                         * 100
                     ),
                 )
 
             case StatusFSM.FACE_DETECTED_LEVEL_B:
-                self.face_frames = []  # full reset
+                self.labels = []  # full reset
                 self._set_status_and_progress(
                     status=StatusFSM.FACE_DETECTED_LEVEL_C,
                     progress=int(
                         self.turnstile_settings.gr_level_c
-                        / self.detected_face["side"]
+                        / self.detected_face["side"]  # type: ignore
                         * 100
                     ),
                 )
@@ -221,6 +237,7 @@ class PostProcessing:
         match status:
             case StatusFSM.NOT_ACTIVE:
                 if turnstile_settings := self.server.turnstile_active():
+                    self.svm_model.download(server=self.server)
                     self._set_turnstile_settings_update(
                         turnstile_settings_update=turnstile_settings
                     )
@@ -240,7 +257,7 @@ class PostProcessing:
                     status=StatusFSM.DETECTING,
                 )
             case StatusFSM.QRCODE_DETECTED | StatusFSM.FACE_DETECTED_LEVEL_B:
-                self.face_frames = []  # full reset
+                self.labels = []  # full reset
                 self._set_status_and_progress(
                     status=StatusFSM.DETECTING,
                 )
@@ -253,7 +270,7 @@ class PostProcessing:
         performance_params = dict()
         performance_params["fps"] = self.fps.get_frames_per_second()
         performance_params["brt"] = self.open_cv.get_brightness(frame=self.frame)
-        performance_params["status"] = self.status.value
+        performance_params["status"] = self.status.value  # type: ignore
 
         self.open_cv.output_performance(
             frame=self.frame,
@@ -263,7 +280,7 @@ class PostProcessing:
         )
 
     def _stage_check_allowed_status(self):
-        passing_time = time.time() - self.passage_time
+        passing_time = time.time() - self.passage_time  # type: ignore
 
         if passing_time >= self.PASSAGE_TIME_LIMIT:
             self._turnstile_close_gate()
@@ -277,7 +294,7 @@ class PostProcessing:
             )
 
     def _stage_check_not_allowed_status(self):
-        passing_time = time.time() - self.passage_time
+        passing_time = time.time() - self.passage_time  # type: ignore
 
         if passing_time >= self.PASSAGE_TIME_LIMIT:
             self._set_status_and_progress(
@@ -324,8 +341,20 @@ class PostProcessing:
 
     # SERVER_PART
     def _server_passage_request(self):
+
+        frequent_label = self._get_frequent_label()
+        if frequent_label is None:
+            # TODO: if server let down
+            print("frequent_label is None")
+            self.passage_time = time.time()
+            self._set_status_and_progress(
+                status=StatusFSM.NOT_ALLOWED,  #  <- There should be an another status
+            )
+            return
+
         passage = self.server.turnstile_passage(
-            frames=self._utils_get_frames_for_request()
+            frequent_label=frequent_label,
+            svm_model_id=self.svm_model.svm_model_id,
         )
 
         if passage is None:
@@ -342,6 +371,7 @@ class PostProcessing:
         self.db.set_passage(
             passage_id=passage.id,
             passage_duration=self._turnstile_passage_duration(),
+            frequent_label=frequent_label
         )
 
     # TURNSTILE_PART
@@ -359,7 +389,7 @@ class PostProcessing:
 
     def _turnstile_passage_duration(self):
         passage_end = time.time()
-        passage_duration = int(self.passage_start - passage_end)
+        passage_duration = int(self.passage_start - passage_end)  # type: ignore
         return passage_duration
 
     def _turstile_open_gate(self):
@@ -372,29 +402,25 @@ class PostProcessing:
     # UTILS_PART
     def _utils_set_face_frames_count(self):
         self.passage_start = time.time()
-        self.FACE_FRAMES_COUNT = int(
-            self.fps.get_frames_per_second() * 0.8
-        )  # <- 80% of perfomance in 1 sec
 
-    def _utils_get_frames_for_request(self):
-        print("_utils_get_frames_for_request")
-        k = int(len(self.face_frames) * 0.25)
-        faces = random.sample(population=self.face_frames[3:], k=k)
-        frames = []
-        request_time = time.time()
-        
-        for i, face in enumerate(faces):
-            ext, buf = cv.imencode(f"frame{i}.png", face)
-            if ext:
-                bytes_io = io.BytesIO(buf)
-                file_name = f"{request_time}-{i}.png"
-                with open(f"{BASEDIR.parent / "data" / "passages" / file_name}", "wb") as f:
-                    f.write(bytes_io.getvalue())
-                frames.append(
-                    (
-                        "frames",
-                        (f"frame{i}.png", bytes_io.getvalue(), "image/png"),
-                    )
-                )
+    def _save_frame(self, cv_rgb, name):
+        ext, buf = cv.imencode(f"frame{name}.png", cv_rgb)
+        if ext:
+            bytes_io = io.BytesIO(buf.tobytes())
+            file_name = f"{name}.png"
+            file_path = BASEDIR.parent / "data" / "passages" / file_name
+            with open(f"{file_path}", "wb") as f:
+                f.write(bytes_io.getvalue())
 
-        return frames
+    def _get_frequent_label(self):
+        collected_labels = self.labels
+        self.labels = []
+        frequent_label = max(set(collected_labels), key=collected_labels.count)
+        required_quantity = int(
+            self.MAX_LABELS_COUNT * self.FREQUENT_LABEL_PERCENT / 100
+        )
+        return (
+            frequent_label
+            if collected_labels.count(frequent_label) >= required_quantity
+            else None
+        )
