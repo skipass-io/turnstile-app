c7784cba2d2c6973b67f89089445c0cfcbcbb6d9
3agoskin <kirillzagoskin.work@gmail.com>
2025-04-14 12:32:40 +0300

    feat(src/core/scaffold.sql): new table `turnstile_svm_models`, new column `frequent_label` in `turnstile_passages`

--- src/core/scaffold.sql
+++ src/core/scaffold.sql
@@ -7,5 +7,11 @@ CREATE TABLE turnstile_app(
 CREATE TABLE turnstile_passages(
     id integer primary key autoincrement,
     passage_id numeric not null,
-    passage_duration numeric not null
+    passage_duration numeric not null,
+    frequent_label text not null
+);
+
+CREATE TABLE turnstile_svm_models(
+    id integer primary key autoincrement,
+    svm_model_id numeric not null
 );
