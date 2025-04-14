7710a3f9c252212eebe2428b2fbed7309334aef3
3agoskin <kirillzagoskin.work@gmail.com>
2025-04-14 12:35:05 +0300

    feat(src/guest_recognition/actions/db): add `get_svm_model_id` & `set_svm_model_id` func, and `frequent_label` params in `set_passage`. Ignore type checking

--- src/guest_recognition/actions/db.py
+++ src/guest_recognition/actions/db.py
@@ -5,13 +5,13 @@ class DB:
     def __init__(self):
         pass
 
-    def get_token(self):
-        rows: dict[str, str] = database.fetchall(
+    def get_token(self): 
+        rows: dict[str, str] = database.fetchall( # type: ignore[empty-body]
             table="turnstile_app",
             columns=["token"],
         )
         if rows:
-            return rows[-1].get("token")
+            return rows[-1].get("token") # type: ignore[empty-body]
 
     def set_token(self, token: str):
         database.insert(
@@ -21,11 +21,28 @@ class DB:
             },
         )
 
-    def set_passage(self, passage_id, passage_duration):
+    def set_passage(self, passage_id, passage_duration, frequent_label):
         database.insert(
             table="turnstile_passages",
             column_values={
                 "passage_id": passage_id,
                 "passage_duration": passage_duration,
+                "frequent_label": frequent_label
+            },
+        )
+
+    def get_svm_model_id(self):
+        rows: dict[str, str] = database.fetchall( # type: ignore[empty-body]
+            table="turnstile_svm_models",
+            columns=["svm_model_id"],
+        )
+        if rows:
+            return int(rows[-1].get("svm_model_id")) # type: ignore[empty-body]
+
+    def set_svm_model_id(self, svm_model_id: int):
+        database.insert(
+            table="turnstile_svm_models",
+            column_values={
+                "svm_model_id": svm_model_id,
             },
         )
